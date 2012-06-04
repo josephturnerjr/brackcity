e10e3568ed93d31233773c8fdde7b3330c7acbee
Joseph Turner <turner@miserware.com>
2012-06-03 20:44:41 -0400

    work work

--- brackcity/schema.sql
+++ brackcity/schema.sql
@@ -1,7 +1,33 @@
 drop table if exists users;
 create table users (
   id integer primary key autoincrement,
-  name string not null,
-  username string not null,
-  pw_hash string not null
+  name TEXT not null,
+  username TEXT not null,
+  pw_hash TEXT not null
+);
+drop table if exists contests;
+create table contests (
+  id integer primary key autoincrement,
+  user_id integer not null,
+  name TEXT not null
+);
+drop table if exists players;
+create table players(
+  id integer primary key autoincrement,
+  contest_id integer not null,
+  name TEXT not null,
+  user_id integer
+);
+drop table if exists games;
+create table games(
+  id integer primary key autoincrement,
+  date TEXT not null,
+  contest_id integer
+);
+drop table if exists scores;
+create table scores(
+  id integer primary key autoincrement,
+  game_id integer not NULL,
+  player_id integer not NULL,
+  score REAL not null
 );
--- brackcity/views.py
+++ brackcity/views.py
@@ -1,5 +1,6 @@
 from brackcity import app
-from flask import request, redirect, url_for, render_template, g, session
+from flask import (request, redirect, url_for,
+                   render_template, g, session, abort)
 import sqlite3
 from contextlib import closing
 from passlib.hash import sha256_crypt
@@ -41,6 +42,11 @@ def check_pw(password, h):
     return sha256_crypt.verify(password, h)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 @app.route('/')
 def main():
     return render_template('base.html')
@@ -56,13 +62,14 @@ def logout():
 def login():
     username = str(request.form['username'])
     password = str(request.form['password'])
-    q = query_db('select name, pw_hash from users where username=?',
+    q = query_db('select id, name, pw_hash from users where username=?',
                  (username,),
                  one=True)
     if q:
         if check_pw(password, q['pw_hash']):
             session['logged_in'] = True
-            session['name'] = q['name']
+            session['user_name'] = q['name']
+            session['user_id'] = q['id']
             return redirect(url_for('main'))
     return redirect(url_for('login'))
 
@@ -85,28 +92,55 @@ def create_user():
         return redirect(url_for('main'))
 
 
+@app.route('/user/<user_id>')
+def user(user_id):
+    user_id = int(user_id)
+    user = query_db('select name, id from users where id=?',
+                 (user_id,), one=True)
+    if not user:
+        abort(404)
+    contests = query_db('select id, name from contests where user_id=?',
+                 (user_id,))
+    return render_template('user.html', user=user, contests=contests)
+
+
+@app.route('/contest', methods=['POST', 'GET'])
+def create_contest():
+    if request.method == 'POST':
+        name = str(request.form['contest_name'])
+        user_id = int(request.form['user_id'])
+        g.db.execute("""insert into contests (user_id, name)
+                                   values (?, ?)""",
+                     (user_id, name))
+        g.db.commit()
+        return redirect(url_for('user', user_id=user_id))
+    else:
+        return render_template('create_contest.html')
+
+
+@app.route('/contest/<contest_id>', methods=['POST', 'GET'])
+def contest(contest_id):
+    contest_id = int(contest_id)
+    contest = query_db('select name, id, user_id from contests where id=?',
+                 (contest_id,), one=True)
+    if not contest:
+        abort(404)
+    players = query_db('select id, name, user_id from players where contest_id=?',
+                       (contest_id,))
+    games = query_db('select id, date from games where contest_id=?',
+                     (contest_id,))
+    return render_template('contest.html', contest=contest, players=players, games=games)
+
+
 @app.route('/game', methods=['POST', 'GET'])
 def add_game():
-    if request.method == 'POST':
-        with open('games.json', 'r') as f:
-            obj = json.loads(f.read())
-        if request.form["winner"] != request.form["loser"]:
-            obj["games"].append([request.form["winner"],
-                                 request.form["loser"]])
-        with open('games.json', 'w') as f:
-            obj = f.write(json.dumps(obj))
-        return redirect(url_for('main'))
-    else:
-        with open('players.json', 'r') as f:
-            obj = json.loads(f.read())
-        players_options = "".join(["<option value='%s'>%s</option>" % (name, name) for name in obj["players"]])
-        return """<form action='/game' method='post'>
-                  <label>Winner</label>
-                  <select name='winner'>%s</select>
-                  <label>Loser</label>
-                  <select name='loser'>%s</select>
-                  <input type='submit' value='Add game'>
-                  </form>""" % (players_options, players_options)
+    return """<form action='/game' method='post'>
+              <label>Winner</label>
+              <select name='winner'>%s</select>
+              <label>Loser</label>
+              <select name='loser'>%s</select>
+              <input type='submit' value='Add game'>
+              </form>""" % ("1", "2")
 
 if __name__ == "__main__":
     app.run(debug=True, port=3456)
