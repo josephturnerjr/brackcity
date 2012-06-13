e457a7c7bc3f8f242a216cc775c92364789c5cf9
Joseph Turner <turner@miserware.com>
2012-06-12 22:49:10 -0400

    Fixed the tests after the session management addition

--- tests/test_users.py
+++ tests/test_users.py
@@ -14,11 +14,14 @@ class TestUsers(unittest.TestCase):
                    "username": self.username,
                    "password": self.password}
         self.id = requests.post(BASE + "/users", data=nu_data).json["data"]["id"]
+        good = {"username": self.username, "password": self.password}
+        r = requests.post(BASE + "/login", data=good)
+        self.auth = (r.json["data"]["session_token"], "foo")
 
     def test_user_creation(self):
         user_list = requests.get(BASE + "/users").json["data"]["users"]
         assert(self.username in map(lambda x: x["username"], user_list))
-        assert(self.name == requests.get(BASE + "/users/%s" % self.id).json["data"]["user"]["name"])
+        assert(self.name == requests.get(BASE + "/users/%s" % self.id, auth=self.auth).json["data"]["user"]["name"])
 
     def test_login(self):
         good = {"username": self.username, "password": self.password}
@@ -45,19 +48,22 @@ class TestUserContests(unittest.TestCase):
                    "username": self.username,
                    "password": self.password}
         requests.post(BASE + "/users", data=nu_data).json
+        good = {"username": self.username, "password": self.password}
+        r = requests.post(BASE + "/login", data=good)
+        self.auth = (r.json["data"]["session_token"], "foo")
 
     def test_users(self):
-        assert(self.username in map(lambda x: x["username"], requests.get(BASE + "/users").json["data"]["users"]))
+        assert(self.username in map(lambda x: x["username"], requests.get(BASE + "/users", auth=self.auth).json["data"]["users"]))
 
     def test_user(self):
         users = requests.get(BASE + "/users").json["data"]["users"]
         my_id = filter(lambda x: x["username"] == self.username, users)[0]["id"]
-        r = requests.get(BASE + "/users/%s" % (my_id))
+        r = requests.get(BASE + "/users/%s" % (my_id), auth=self.auth)
         assert(r.status_code == 200)
         assert(r.json["data"]["user"]["name"] == self.name)
-        r = requests.get(BASE + "/users/%s" % ("NOTANID"))
+        r = requests.get(BASE + "/users/%s" % ("NOTANID"), auth=self.auth)
         assert(r.status_code == 404)
-        r = requests.get(BASE + "/users/%s" % (0))
+        r = requests.get(BASE + "/users/%s" % (0), auth=self.auth)
         assert(r.status_code == 404)
 
 
