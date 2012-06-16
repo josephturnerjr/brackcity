2161efc7440f8c9bd908acefea5542b6b78cd5fc
Joseph Turner <turner@miserware.com>
2012-06-16 00:48:32 -0400

    New tests for other verbs

--- tests/test_contests.py
+++ tests/test_contests.py
@@ -6,7 +6,7 @@ from utils import BASE, create_user
 
 
 
-class TestUserContest(unittest.TestCase):
+class TestUserContests(unittest.TestCase):
     def setUp(self):
         self.username = uuid.uuid4().hex
         self.password = uuid.uuid4().hex
@@ -19,6 +19,15 @@ class TestUserContest(unittest.TestCase):
         r = requests.post(BASE + "/login", data=good)
         self.auth = (r.json["data"]["session_token"], "foo")
 
+    def test_list(self):
+        r = requests.get(BASE + "/users/%s/contests" % self.id, auth=self.auth).json
+        assert("contests" in r["data"])
+        assert(len(r["data"]["contests"]) == 0)
+        r = requests.post(BASE + "/users/%s/contests" % self.id, data={"name": "boo"}, auth=self.auth)
+        r = requests.get(BASE + "/users/%s/contests" % self.id, auth=self.auth).json
+        assert("contests" in r["data"])
+        assert(len(r["data"]["contests"]) == 1)
+        
     def test_create(self):
         r = requests.post(BASE + "/users/%s/contests" % self.id)
         assert(r.status_code == 401)
@@ -33,6 +42,42 @@ class TestUserContest(unittest.TestCase):
                    "username": username,
                    "password": password}
         id = create_user(**nu_data)["id"]
+        r = requests.post(BASE + "/users/%s/contests" % id, data={"name": "boo"}, auth=self.auth)
+        assert(r.status_code == 403)
+
+    def test_details(self):
+        r = requests.post(BASE + "/users/%s/contests" % self.id, data={"name": "boo"}, auth=self.auth).json
+        contest_id = r["data"]["id"]
+        r = requests.get(BASE + "/users/%s/contests/%s" % (self.id, contest_id), auth=self.auth).json["data"]
+        assert("user_id" in r["contest"])
+        assert("name" in r["contest"])
+        assert("id" in r["contest"])
+        r = requests.get(BASE + "/users/%s/contests/%s" % (self.id, 9234923493), auth=self.auth)
+        assert(r.status_code == 404)
+
+    def test_delete(self):
+        r = requests.post(BASE + "/users/%s/contests" % self.id, data={"name": "boo"}, auth=self.auth).json
+        contest_id = r["data"]["id"]
+        r = requests.get(BASE + "/users/%s/contests/%s" % (self.id, contest_id), auth=self.auth)
+        assert(r.status_code == 200)
+        r = requests.delete(BASE + "/users/%s/contests/%s" % (self.id, contest_id), auth=self.auth)
+        assert(r.status_code == 200)
+        r = requests.get(BASE + "/users/%s/contests/%s" % (self.id, contest_id), auth=self.auth)
+        assert(r.status_code == 404)
+        r = requests.delete(BASE + "/users/%s/contests/%s" % (self.id, 239482), auth=self.auth)
+        assert(r.status_code == 404)
+        username = uuid.uuid4().hex
+        password = uuid.uuid4().hex
+        name = "Joseph Tester"
+        nu_data = {"name": name,
+                   "username": username,
+                   "password": password}
+        id = create_user(**nu_data)["id"]
+        good = {"username": username, "password": password}
+        r = requests.post(BASE + "/login", data=good)
+        auth = (r.json["data"]["session_token"], "foo")
+        r = requests.post(BASE + "/users/%s/contests" % id, data={"name": "boo"}, auth=auth)
+        assert(r.status_code == 200)
         r = requests.post(BASE + "/users/%s/contests" % id, data={"name": "boo"}, auth=self.auth)
         assert(r.status_code == 403)
 
